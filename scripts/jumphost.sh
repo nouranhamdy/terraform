ba3089c607abcfbd0710b249fe080d1602a98d34
nouranhamdy <nouranhamdy4555@gmail.com>
2022-05-28 23:06:59 +0200

    Ansible dir for change directory

--- scripts/jumphost.sh
+++ scripts/jumphost.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /var/jenkins_home/workspace/test-terraform/
-sudo cat terraform/key-pair.pem > ~/.ssh/mykey.pem
+cat terraform/key-pair.pem > ~/.ssh/mykey.pem
 chmod 400 ~/.ssh/mykey.pem
 #cd terraform/
 #bastion_ip=`terraform output -json bastion-ip | tr -d '"'`
