b1b0e070fbd31d5472a20802ed33831b950dc99c
nouranhamdy <nouranhamdy4555@gmail.com>
2022-05-29 03:41:13 +0200

    main route table

--- terraform/network/vpc.tf
+++ terraform/network/vpc.tf
@@ -4,3 +4,21 @@ resource "aws_vpc" "vpc-01" {
     Name = "vpc-01"
   }
 }
+
+resource "aws_route_table" "default-route" {
+  vpc_id = aws_vpc.vpc-01.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw-01.id
+  }
+
+  tags = {
+    Name = "default-route"
+  }
+}
+
+resource "aws_main_route_table_association" "a" {
+  vpc_id         = aws_vpc.vpc-01.id
+  route_table_id = aws_route_table.default-route.id
+}
