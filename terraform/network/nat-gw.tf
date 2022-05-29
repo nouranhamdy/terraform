c37419fd1f1cf5caccb524397126e7b2e8155f47
nouranhamdy <nouranhamdy4555@gmail.com>
2022-05-30 00:43:53 +0200

    terraform edit

--- terraform/network/nat-gw.tf
+++ terraform/network/nat-gw.tf
@@ -14,3 +14,21 @@ resource "aws_nat_gateway" "nat-gw" {
   # on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateway.igw-01]
 }
+
+
+resource "aws_eip" "nateIP-02" {
+   vpc   = true
+ }
+
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = aws_eip.nateIP-02.id
+  subnet_id     = aws_subnet.pub-subnet-02.id
+
+  tags = {
+    Name = "nat-gw-02"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw-01]
+}
