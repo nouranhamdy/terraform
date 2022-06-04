f20e675873d8962ed0f0d85c7248c05c2676f494
nouranhamdy <nouranhamdy4555@gmail.com>
2022-06-04 10:18:33 +0200

    edit security group

--- terraform/sgr.tf
+++ terraform/sgr.tf
@@ -13,9 +13,9 @@ resource "aws_security_group" "sgr-ssh" {
   }
 
   egress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
@@ -40,9 +40,9 @@ resource "aws_security_group" "sgr-ssh-8000" {
   }
 
   egress {
-    from_port        = 8000
-    to_port          = 8000
-    protocol         = "tcp"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
     cidr_blocks      = [module.network.vpc-cidr-block]
   }
 
