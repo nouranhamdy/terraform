5071fed01e31642451ee2f4e39c7e3861accd37b
nouranhamdy <nouranhamdy4555@gmail.com>
2022-06-05 18:49:01 +0200

    java jar download in ansible

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,23 +1,23 @@
-resource "aws_instance" "ec2" {
-  ami           = var.ami # us-west-2
-  instance_type = "t2.micro"
-  key_name = aws_key_pair.public_key.key_name
+# resource "aws_instance" "ec2" {
+#   ami           = var.ami # us-west-2
+#   instance_type = "t2.micro"
+#   key_name = aws_key_pair.public_key.key_name
 
-  network_interface {
-    network_interface_id = aws_network_interface.nic-ec2.id
-    device_index         = 0
-  }
+#   network_interface {
+#     network_interface_id = aws_network_interface.nic-ec2.id
+#     device_index         = 0
+#   }
 
-  tags = {
-    Name = "private-instance"
-  }
+#   tags = {
+#     Name = "private-instance"
+#   }
 
-}
+# }
 
-resource "aws_network_interface" "nic-ec2" {
-  subnet_id       = module.network.private-subnet-01-id
-  private_ips     = ["10.0.1.20"]
-  security_groups = [aws_security_group.sgr-ssh-8000.id]
+# resource "aws_network_interface" "nic-ec2" {
+#   subnet_id       = module.network.private-subnet-01-id
+#   private_ips     = ["10.0.1.20"]
+#   security_groups = [aws_security_group.sgr-ssh-8000.id]
 
-}
+# }
 
