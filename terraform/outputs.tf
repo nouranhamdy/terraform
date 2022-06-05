5645f2766ba0bfd94f5199e69613cb5a541fd3e3
nouranhamdy <nouranhamdy4555@gmail.com>
2022-06-05 18:51:39 +0200

    java jar download in ansible

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,10 @@ output "bastion-ip" {
   value = aws_instance.bastion.public_ip
 }
 
-output "private-ip" {
-  value = aws_instance.ec2.private_ip
-}
+# output "private-ip" {
+#   value = aws_instance.ec2.private_ip
+# }
 
-output "lb_dns" {
-  value = aws_lb.loadbalancer.dns_name
-}
+# output "lb_dns" {
+#   value = aws_lb.loadbalancer.dns_name
+# }
