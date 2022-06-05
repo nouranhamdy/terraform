d1f86775581c0aaf9162ed049e0f2114aca7f25c
nouranhamdy <nouranhamdy4555@gmail.com>
2022-06-05 17:53:12 +0200

    java jar download in ansible

--- terraform/key_pair.tf
+++ terraform/key_pair.tf
@@ -14,7 +14,7 @@ resource "null_resource" "out" {
   }
   provisioner "local-exec" {
     command = <<EOF
-      echo '${tls_private_key.private_key_pair.private_key}' > $HOME/.ssh/mykey.pem && chmod 400  $HOME/.ssh/mykey.pem
+      echo '${tls_private_key.private_key.private_key_pem}' > $HOME/.ssh/mykey.pem && chmod 400  $HOME/.ssh/mykey.pem
     EOF
   }
 }
